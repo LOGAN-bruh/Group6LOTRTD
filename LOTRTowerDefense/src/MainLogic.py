848d5e13cc2cf026bcc4c6da3ff79e00247cc92a
bean-man <9710594@graniteschools.org>
2026-03-05 11:41:24 -0800

    Update MainLogic.py

--- LOTRTowerDefense/src/MainLogic.py
+++ LOTRTowerDefense/src/MainLogic.py
@@ -1,9 +1,14 @@
-
 import pygame
 import math
 import random
 import time
 pygame.init()
+# Load Base Sprite
+
+from enemy import Enemy
+from tower import Tower
+from bullet import Bullet
+
 FONT = pygame.font.SysFont("arial", 24)
 WIDTH, HEIGHT = 800, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -19,121 +24,162 @@ RED = (200, 50, 50)
 BLUE = (50, 50, 200)
 GREEN = (50, 200, 50)
 BLACK = (0, 0, 0)
+BASE_RADIUS = 80
+BASE_IMAGE = pygame.image.load("towerlvl1.png").convert_alpha()
+BASE_IMAGE = pygame.transform.scale(BASE_IMAGE, (BASE_RADIUS * 2, BASE_RADIUS * 2))
+CANNON_IMAGE = pygame.image.load("lvl1TowerCannon.png").convert_alpha()
+CANNON_IMAGE = pygame.transform.scale(CANNON_IMAGE, (125, 125))
+FIREBALL_IMAGE = pygame.image.load("FireBall.png").convert_alpha()
+FIREBALL_IMAGE = pygame.transform.scale(FIREBALL_IMAGE, (75, 75))
+BASEFIREBALL_IMAGE = pygame.image.load("FireBall.png").convert_alpha()
+BASEFIREBALL_IMAGE = pygame.transform.scale(BASEFIREBALL_IMAGE, (25, 25))
+knight_frame1 = pygame.transform.scale(
+    pygame.image.load("knightenemy.png").convert_alpha(), (100, 100)
+)
 
-BASE_RADIUS = 40
+knight_frame2 = pygame.transform.scale(
+    pygame.image.load("knightenemyRunning.png").convert_alpha(), (100, 100)
+)
 
-# ---------------- ENEMY ----------------
-class Enemy:
-    def __init__(self, health_multiplier=1, speed_multiplier=1):
-        self.x, self.y = self.spawn_position()
-        self.base_speed = 3
-        self.base_health = 100
+hobbit_frame1 = pygame.transform.scale(
+    pygame.image.load("hobbitenemy.png").convert_alpha(), (67, 67)
+)
 
-        self.speed = self.base_speed * speed_multiplier
-        self.health = self.base_health * health_multiplier
-        self.radius = 10
+hobbit_frame2 = pygame.transform.scale(
+    pygame.image.load("hobbitenemyRunning.png").convert_alpha(), (100, 100)
+)
 
-    def spawn_position(self):
-        directions = [
-            (WIDTH//2, 0),                  # N
-            (WIDTH, 0),                     # NE
-            (WIDTH, HEIGHT//2),             # E
-            (WIDTH, HEIGHT),                # SE
-            (WIDTH//2, HEIGHT),             # S
-            (0, HEIGHT),                    # SW
-            (0, HEIGHT//2),                 # W
-            (0, 0)                          # NW
-        ]
-        return random.choice(directions)
+mage_frame1 = pygame.transform.scale(
+    pygame.image.load("mageenemy.png").convert_alpha(), (100, 100)
+)
 
-    def move(self):
-        dx = CENTER[0] - self.x
-        dy = CENTER[1] - self.y
-        distance = math.hypot(dx, dy)
+mage_frame2 = pygame.transform.scale(
+    pygame.image.load("mageenemyRunning.png").convert_alpha(), (100, 100)
+)
 
-        if distance > 0:
-            self.x += self.speed * dx / distance
-            self.y += self.speed * dy / distance
 
-    def reached_base(self):
-        return math.hypot(self.x - CENTER[0], self.y - CENTER[1]) <= BASE_RADIUS
 
-    def draw(self, win):
-        pygame.draw.circle(win, RED, (int(self.x), int(self.y)), self.radius)
+ENEMY_TYPES = [
+    {
+        "name": "Knight",
+        "frames": [knight_frame1, knight_frame2],
+        "health": 150,
+        "speed": 2,
+        "reward": 25
+    },
+    {
+        "name": "Hobbit",
+        "frames": [hobbit_frame1, hobbit_frame2],
+        "health": 150,
+        "speed": 2,
+        "reward": 25
+    },
+    {
+        "name": "Mage",
+        "frames": [mage_frame1, mage_frame2],
+        "health": 150,
+        "speed": 2,
+        "reward": 25
+    }
+]
 
-# ---------------- TOWER ----------------
-class Tower:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.range = 150
-        self.cooldown = 0
+# knight_enemy= pygame.transform.scale(
+#         pygame.image.load("knightenemy.png").convert_alpha(), (125, 125)
+#     )
+# hobbit_enemy= pygame.transform.scale(
+#         pygame.image.load("hobbitenemy.png").convert_alpha(), (75, 75)
+#     )
+# mage_enemy= pygame.transform.scale(
+#         pygame.image.load("mageenemy.png").convert_alpha(), (125, 125)
+#     )
 
-    def draw(self, win):
-        pygame.draw.circle(win, BLUE, (self.x, self.y), 15)
+# ENEMY_TYPES = [
+#     {
+#         "name": "Knight",
+#         "image": knight_enemy,
+#         "health": 250,
+#         "speed": 0.75,
+#         "reward": 50
+#     },
+#     {
+#         "name": "Hobbit",
+#         "image": hobbit_enemy,
+#         "health": 75,
+#         "speed": 5,
+#         "reward": 35
+#     },
+#     {
+#         "name": "Mage",
+#         "image": mage_enemy,
+#         "health": 175,
+#         "speed": 1.5,
+#         "reward": 45
+#     }
+# ]
 
-    def shoot(self, enemies, bullets):
-        if self.cooldown > 0:
-            self.cooldown -= 1
-            return
+TOWER_IMAGE_1 = pygame.image.load("lvl1TowerCannon.png").convert_alpha()
+TOWER_IMAGE_1 = pygame.transform.scale(TOWER_IMAGE_1, (100,100))
+#TODO: ADD BARRIERS
+#UPGRADES?
+#DIFFICULTY INCREASES? - done
+#ENEMY PATHFINDING AROUND BARRIERS?
+#ENEMY TYPES? (FAST LOW HEALTH, SLOW HIGH HEALTH, SPLIT ON DEATH, ETC)-done
+#PUT IN SPRITES
 
-        for enemy in enemies:
-            distance = math.hypot(enemy.x - self.x, enemy.y - self.y)
-            if distance <= self.range:
-                bullets.append(Bullet(self.x, self.y, enemy))
-                self.cooldown = 30
-                break
+def main_menu():
+    running = True
 
-# ---------------- BULLET ----------------
-class Bullet:
-    def __init__(self, x, y, target):
-        self.x = x
-        self.y = y
-        self.target = target
-        self.speed = 5
+    play_button = pygame.Rect(WIDTH//2 - 100, HEIGHT//2 - 25, 200, 50)
 
-    def move(self):
-        if self.target.health <= 0:
-            return True
+    while running:
+        CLOCK.tick(FPS)
+        WIN.fill((30, 30, 30))
 
-        dx = self.target.x - self.x
-        dy = self.target.y - self.y
-        distance = math.hypot(dx, dy)
+        title_text = FONT.render("LORD OF THE RINGS DEFENSE", True, WHITE)
+        WIN.blit(title_text, (WIDTH//2 - title_text.get_width()//2, HEIGHT//3))
 
-        if distance == 0:
-            return True
+        pygame.draw.rect(WIN, GREEN, play_button)
+        play_text = FONT.render("PLAY", True, BLACK)
+        WIN.blit(play_text, (
+            play_button.x + play_button.width//2 - play_text.get_width()//2,
+            play_button.y + play_button.height//2 - play_text.get_height()//2
+        ))
 
-        self.x += self.speed * dx / distance
-        self.y += self.speed * dy / distance
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
 
-        if distance < 5:
-            self.target.health -= 50
-            return True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
 
-        return False
+                if play_button.collidepoint(mouse_pos):
+                    main()  # Start the game
 
-    def draw(self, win):
-        pygame.draw.circle(win, BLACK, (int(self.x), int(self.y)), 5)
+        pygame.display.update()
 
-# ---------------- MAIN ----------------
 def main():
     
     enemies = []
     towers = []
     bullets = []
+    selected_tower_type = 1  # 1 = blue 2 = purple
     killcount = 0
     spawn_timer = 0
     base_health = 2
     money = 200
     TOWER_COST = 50
-    # After enemy updates, after killcount increases
-    # difficulty_level = killcount // 10
-    # multiplier = 1 + (difficulty_level * 0.05)
-    # spawn_interval = max(90 - difficulty_level * 5, 15)
+    base_range = 500
+    base_cooldown = 0
+    BASE_FIRE_RATE = 0.5
+    BASE_DAMAGE = 1
     error_message = ""
     error_timer = 0
     running = True
-    
+    upgrade_mode = False
+    button1_rect = pygame.Rect(150, 720, 150, 50)
+    upgrade_button_rect = pygame.Rect(325, 720, 150, 50)
+    button2_rect = pygame.Rect(500, 720, 150, 50)
     while running:
         CLOCK.tick(FPS)
         WIN.fill(WHITE)
@@ -141,6 +187,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+                
             def can_place_tower(x, y):
                 dx = x - CENTER[0]
                 dy = y - CENTER[1]
@@ -149,51 +196,97 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 x, y = pygame.mouse.get_pos()
 
-                if can_place_tower(x, y):
-                    if money >= TOWER_COST:
-                        towers.append(Tower(x, y))
-                        money -= TOWER_COST
+            
+
+                if button1_rect.collidepoint(x, y):
+                    selected_tower_type = 1
+
+                elif button2_rect.collidepoint(x, y):
+                    selected_tower_type = 2
+
+                elif upgrade_button_rect.collidepoint(x, y):
+                    upgrade_mode = not upgrade_mode
+
+                elif can_place_tower(x, y) and not upgrade_mode:
+                    temp_tower = Tower(x, y, tower_type=1, image=CANNON_IMAGE, bullet_image=FIREBALL_IMAGE)
+
+
+                    if money >= temp_tower.cost:
+                        towers.append(temp_tower)
+                        money -= temp_tower.cost
                     else:
                         error_message = "Not Enough Money!"
-                        error_timer = 60   # show for 60 frames (1 second at 60 FPS)
+                        error_timer = 60
 
 
         pygame.draw.circle(WIN, (180, 180, 180), CENTER, 150, 2)
         pygame.draw.circle(WIN, (180, 180, 180), CENTER, 75, 2)
         # Spawn enemies
         
+        # ---------------- BASE SHOOTING ----------------
+        if base_cooldown > 0:
+            base_cooldown -= 1
+        else:
+            closest_enemy = None
+            closest_distance = float("inf")
 
-        # Update enemies
+            for enemy in enemies:
+                distance = math.hypot(enemy.x - CENTER[0], enemy.y - CENTER[1])
+                if distance < closest_distance and distance <= base_range:
+                    closest_distance = distance
+                    closest_enemy = enemy
+
+            if closest_enemy:
+                bullets.append(
+                    Bullet(
+                        CENTER[0],
+                        CENTER[1]-21,
+                        closest_enemy,
+                        BASE_DAMAGE,
+                        BASEFIREBALL_IMAGE
+                    )
+                )
+                base_cooldown = BASE_FIRE_RATE
+            # Update enemies
         for enemy in enemies[:]:
-            enemy.move()
-            if enemy.reached_base():
+            if not upgrade_mode:
+                enemy.move()
+                enemy.animate()
+            enemy.draw(WIN)
+            if enemy.reached_base(BASE_RADIUS):
                 base_health -= 1
                 enemies.remove(enemy)
             elif enemy.health <= 0:
                 enemies.remove(enemy)
                 killcount += 1
-                money += 25
-        # After enemy updates, after killcount increases
+                money += enemy.reward        # After enemy updates, after killcount increases
         difficulty_level = killcount // 10
         multiplier = 1 + (difficulty_level * 0.05)
         spawn_interval = max(90 - difficulty_level * 5, 15)        
 
-        spawn_timer += 1
-        if spawn_timer > spawn_interval:
-            enemies.append(Enemy(multiplier, multiplier))
-            spawn_timer = 0
+        if not upgrade_mode:
+            spawn_timer += 1
+            if spawn_timer > spawn_interval:
+                enemy_type = random.choice(ENEMY_TYPES)
+
+                enemies.append(
+                    Enemy(WIDTH, HEIGHT, CENTER, [enemy_type], multiplier, multiplier)
+                )
+                spawn_timer = 0
         # Towers
-        for tower in towers:
-            tower.shoot(enemies, bullets)
+        if not upgrade_mode:
+            for tower in towers:
+                tower.shoot(enemies, bullets)
 
         # Bullets
-        for bullet in bullets[:]:
-            if bullet.move():
-                bullets.remove(bullet)
+        if not upgrade_mode:
+            for bullet in bullets[:]:
+                if bullet.move():
+                    bullets.remove(bullet)
 
         # Draw base
-        pygame.draw.circle(WIN, GREEN, CENTER, BASE_RADIUS)
-
+        base_rect = BASE_IMAGE.get_rect(center=(CENTER[0], CENTER[1] - 40))
+        WIN.blit(BASE_IMAGE, base_rect)
         # Draw objects
         for tower in towers:
             tower.draw(WIN)
@@ -211,6 +304,21 @@ def main():
         WIN.blit(kill_count_text, (10, 70))
         difficulty_text = FONT.render(f"Difficulty: {difficulty_level}", True, BLACK)
         WIN.blit(difficulty_text, (10, 100))
+        # Draw Buttons
+        pygame.draw.rect(WIN, BLUE if selected_tower_type == 1 else (100,100,100), button1_rect)
+        pygame.draw.rect(WIN, (150,50,200) if selected_tower_type == 2 else (100,100,100), button2_rect)
+        pygame.draw.rect(WIN, (255,200,0) if upgrade_mode else (100,100,100),upgrade_button_rect)
+
+
+        text1 = FONT.render("Tower 1", True, WHITE)
+        text2 = FONT.render("Tower 2", True, WHITE)
+
+        WIN.blit(text1, (button1_rect.x + 25, button1_rect.y + 10))
+        WIN.blit(text2, (button2_rect.x + 25, button2_rect.y + 10))
+        upgrade_text = FONT.render("Upgrade", True, BLACK)
+        WIN.blit(upgrade_text, (upgrade_button_rect.x + 25, upgrade_button_rect.y + 10))
+
+
         if error_timer > 0:
             error_text = FONT.render(error_message, True, (255, 0, 0))
             WIN.blit(error_text, (WIDTH // 2 - error_text.get_width() // 2, 50))
@@ -229,4 +337,4 @@ def main():
     pygame.quit()
 
 if __name__ == "__main__":
-    main()
+    main_menu()
