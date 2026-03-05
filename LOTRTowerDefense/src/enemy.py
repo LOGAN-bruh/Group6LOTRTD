8b415e64ad3a21d9e6327cba11029331ca855881
bean-man <9710594@graniteschools.org>
2026-03-05 10:40:57 -0800

    Update enemy.py

--- LOTRTowerDefense/src/enemy.py
+++ LOTRTowerDefense/src/enemy.py
@@ -2,23 +2,49 @@ import pygame
 import math
 import random
 
+
+import random
+import math
+import pygame
+
 class Enemy:
-    def __init__(self, width, height, center, enemy_image,
+    def __init__(self, width, height, center, enemy_types,
                  health_multiplier=1, speed_multiplier=1):
 
         self.WIDTH = width
         self.HEIGHT = height
         self.CENTER = center
-        self.image = enemy_image
+
+        # choose random type from what main passed in
+
+        # Pick the enemy type
+        enemy_type = random.choice(enemy_types)
+
+        self.frames = enemy_type["frames"]
+        self.current_frame = 0
+        self.animation_timer = 0
+        self.animation_speed = 5
+
+        self.image = self.frames[self.current_frame]  # initial frame
+
+
+        self.name = enemy_type["name"]
+        self.base_health = enemy_type["health"]
+        self.base_speed = enemy_type["speed"]
+        self.reward = enemy_type["reward"]
+
+        self.health = self.base_health * health_multiplier
+        self.speed = self.base_speed * speed_multiplier
 
         self.x, self.y = self.spawn_position()
-        self.base_speed = 3
-        self.base_health = 100
 
-        self.speed = self.base_speed * speed_multiplier
-        self.health = self.base_health * health_multiplier
-        self.radius = 10
-        self.spawn_pos = (self.x, self.y)
+    def animate(self):
+        self.animation_timer += 1
+
+        if self.animation_timer >= self.animation_speed:
+            self.animation_timer = 0
+            self.current_frame = (self.current_frame + 1) % len(self.frames)
+            self.image = self.frames[self.current_frame]
 
     def spawn_position(self):
         directions = [
